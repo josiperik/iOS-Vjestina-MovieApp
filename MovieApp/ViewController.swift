65824aba67062844a5b95ff92b5105b67a8fb241
josiperik <102482734+josiperik@users.noreply.github.com>
2022-03-30 18:35:34 +0200

    add starting screen

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -7,7 +7,7 @@
 import SnapKit
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate {
+class ViewController: UIViewController {
     private var movieImage = UIImageView()
     private var movieTitle = UILabel()
     private var movieDate = UILabel()
@@ -15,10 +15,18 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     private var duration = UILabel()
     private var scorePrecentage = UILabel()
     private var scoreLabel = UILabel()
-    private var overview = UILabel()
+    private var overviewLabel = UILabel()
+    private var MovieDescription = UILabel()
+    private var circleIcon = UIImageView()
     private var starIcon = UIImageView()
-    private var descript = UILabel()
-    private var peopleCollectionView = UICollectionView()
+    private var person1  = UILabel()
+    private var person2  = UILabel()
+    private var person3  = UILabel()
+    private var person4  = UILabel()
+    private var person5  = UILabel()
+    private var person6  = UILabel()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +37,7 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     }
     private func setUpSubviews() {
     
-        movieImage.image = cropToBounds(image: UIImage(named: "iron man")!, width: 363, height: 303)
+        movieImage.image = cropToBounds(image: UIImage(named: "IronMan")!, width: 363, height: 303)
         movieImage.contentMode = .scaleAspectFill
         movieImage.layer.masksToBounds = true
         view.addSubview(movieImage)
@@ -44,6 +52,7 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         movieDate.font = UIFont.systemFont(ofSize: 14)
         view.addSubview(movieDate)
         
+
         genres.text = "Action, Science Fiction, Adventure"
         genres.textColor = .white
         genres.font = UIFont.systemFont(ofSize: 14)
@@ -54,41 +63,115 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         scorePrecentage.font = UIFont.boldSystemFont(ofSize: 15 )
         view.addSubview(scorePrecentage)
         
-        
         scoreLabel.text = "User Score"
         scoreLabel.textColor = .white
         scoreLabel.font = UIFont.boldSystemFont(ofSize: 14)
         view.addSubview(scoreLabel)
         
-        descript.text = "After being held captive in an Afghan cave, /n billionare engineer Tony Stark creates a unique /n weaponized suit of armor to fight evil. "
-        descript.numberOfLines = 0;
-        descript.textColor = .black
-        descript.font = UIFont.systemFont(ofSize: 14)
-        view.addSubview(descript)
+        let attributedString = NSMutableAttributedString(string: "After being held captive in an Afghan cave,\nbillionare engineer Tony Stark creates a unique\nweaponized suit of armor to fight evil. ")
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 5
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
+
+        MovieDescription.attributedText = attributedString
+        MovieDescription.numberOfLines = 0;
+        MovieDescription.textColor = .black
+        MovieDescription.font = UIFont.systemFont(ofSize: 14)
+        view.addSubview(MovieDescription)
         
-        overview.text = "Overwiew"
-        overview.textColor = .black
-        overview.font = UIFont.systemFont(ofSize: 20)
-        view.addSubview(overview)
+        overviewLabel.text = "Overwiew"
+        overviewLabel.textColor = .black
+        overviewLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        view.addSubview(overviewLabel)
         
         duration.text = "2h 6m"
         duration.font = UIFont.boldSystemFont(ofSize: 14)
         duration.textColor = .white
         view.addSubview(duration)
-        
-        starIcon.image = UIImage(systemName: "star.circle")!
-        let config = UIImage.SymbolConfiguration(paletteColors: [.white, .gray])
-        starIcon.backgroundColor = .gray
-        starIcon.preferredSymbolConfiguration = config
-        //starIcon.preferredImageVariantShape = UIImage.VariantShape.circle
-        view.addSubview(starIcon)
-        starIcon.layer.cornerRadius = 16
-        
-        peopleCollectionView.delegate  = self
-        
-        
-    
 
+        circleIcon.image = UIImage(systemName: "circle.fill")!
+        circleIcon.tintColor = .black
+        let config1 = UIImage.SymbolConfiguration(paletteColors: [.darkGray])
+        circleIcon.backgroundColor = .darkGray
+        circleIcon.preferredSymbolConfiguration = config1
+        circleIcon.layer.cornerRadius = 16
+        view.addSubview(circleIcon)
+        
+        starIcon.image = UIImage(systemName: "star")!
+        let config = UIImage.SymbolConfiguration(paletteColors: [.white, .clear,])
+        starIcon.backgroundColor = .clear
+        starIcon.preferredSymbolConfiguration = config
+//        starIcon.preferredImageVariantShape = UIImage.VariantShape.circle
+        view.addSubview(starIcon)
+//        starIcon.layer.cornerRadius = 16
+        
+
+        let attrs = [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 14)]
+        let roleAtributes = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14)]
+        
+        let characterRole = " \nCharacters"
+        let characterString = NSMutableAttributedString(string: characterRole, attributes: roleAtributes)
+        
+        
+        
+        let directorRole = " \nDirector"
+        let directorString = NSMutableAttributedString(string: directorRole, attributes: roleAtributes)
+        
+        let screenPlayRole = " \nScreenplay"
+        let screenplayString = NSMutableAttributedString(string: screenPlayRole, attributes: roleAtributes)
+        
+       
+        
+        let person_1 = "Don Heck"
+        let attributedStrings = NSMutableAttributedString(string:person_1, attributes:attrs)
+        attributedStrings.append(characterString)
+        person1.attributedText = attributedStrings
+        person1.numberOfLines = 0;
+        person1.textColor = .black
+        view.addSubview(person1)
+        
+        let person_2 = "Jack Kirby"
+        let attributedStrings2 = NSMutableAttributedString(string:person_2, attributes:attrs)
+        attributedStrings2.append(characterString)
+        person2.attributedText = attributedStrings2
+        person2.numberOfLines = 0;
+        person2.textColor = .black
+        view.addSubview(person2)
+        
+        let person_3 = "Jon Favreau"
+        let attributedStrings3 = NSMutableAttributedString(string:person_3, attributes:attrs)
+        attributedStrings3.append(directorString)
+        person3.attributedText = attributedStrings3
+        person3.numberOfLines = 0;
+        person3.textColor = .black
+        view.addSubview(person3)
+        
+        let person_4 = "Don Heck"
+        let attributedStrings4 = NSMutableAttributedString(string:person_4, attributes:attrs)
+        attributedStrings4.append(screenplayString)
+        person4.attributedText = attributedStrings4
+        person4.numberOfLines = 0;
+        person4.textColor = .black
+        view.addSubview(person4)
+        
+        let person_5 = "Jack Marcum"
+        let attributedStrings5 = NSMutableAttributedString(string:person_5, attributes:attrs)
+        attributedStrings5.append(screenplayString)
+        person5.attributedText = attributedStrings5
+        person5.numberOfLines = 0;
+        person5.textColor = .black
+        view.addSubview(person5)
+        
+        let person_6 = "Matt Holloway"
+        let attributedStrings6 = NSMutableAttributedString(string:person_6, attributes:attrs)
+        attributedStrings6.append(screenplayString)
+        person6.attributedText = attributedStrings6
+        person6.numberOfLines = 0;
+        person6.textColor = .black
+        view.addSubview(person6)
+        
+        
+        
         
     }
     private func setUpConstraints() {
@@ -102,6 +185,7 @@ class ViewController: UIViewController, UICollectionViewDelegate {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.width.equalToSuperview()
             $0.height.equalTo(303)
+            $0.width.equalTo(363)
            
       }
         
@@ -125,15 +209,58 @@ class ViewController: UIViewController, UICollectionViewDelegate {
             //$0.width.height.equalTo(500)
             
         }
-        scoreLabel.snp_makeConstraints{
+        scoreLabel.snp.makeConstraints{
             $0.centerY.equalTo(scorePrecentage)
             $0.leading.equalTo(scorePrecentage.snp.trailing).offset(25)
             }
         
-        starIcon.snp.makeConstraints{
+   
+        overviewLabel.snp.makeConstraints{
+            $0.top.equalTo(movieImage.snp.bottom).offset(20)
+            $0.leading.equalTo(movieImage).inset(16)
+                                            
+        }
+        MovieDescription.snp.makeConstraints{
+            $0.top.equalTo(overviewLabel.snp.bottom).offset(16)
+            $0.leading.equalTo(movieImage).inset(16)
+        }
+        circleIcon.snp.makeConstraints{
             $0.top.equalTo(genres.snp.bottom).offset(10)
-            $0.leading.equalTo(movieImage).inset(20)
+            $0.leading.equalTo(movieImage).offset(20)
             $0.width.height.equalTo(32)
+        }
+        starIcon.snp.makeConstraints{
+            $0.center.equalTo(circleIcon)
+            $0.width.height.equalTo(15)
+
+        }
+
+        person2.snp.makeConstraints{
+            $0.leading.equalTo(person1.snp.trailing).offset(44)
+            $0.top.equalTo(MovieDescription.snp.bottom).offset(22)
+        }
+       
+        person1.snp.makeConstraints{
+            $0.leading.equalToSuperview().inset(16)
+            $0.top.equalTo(MovieDescription.snp.bottom).offset(22)
+        }
+        person3.snp.makeConstraints{
+            $0.leading.equalTo(person2.snp.trailing).offset(55)
+            $0.top.equalTo(MovieDescription.snp.bottom).offset(22)
+        }
+        
+        person5.snp.makeConstraints{
+            $0.leading.equalTo(person1.snp.trailing).offset(44)
+            $0.top.equalTo(person2.snp.bottom).offset(26)
+        }
+
+        person4.snp.makeConstraints{
+            $0.leading.equalToSuperview().inset(16)
+            $0.top.equalTo(person1.snp.bottom).offset(26)
+        }
+        person6.snp.makeConstraints{
+            $0.leading.equalTo(person2.snp.trailing).offset(55)
+            $0.top.equalTo(person1.snp.bottom).offset(26)
         }
 
 
@@ -172,5 +299,20 @@ class ViewController: UIViewController, UICollectionViewDelegate {
 
             return image
         }
-
+  
 }
+//extension String {
+//func withBoldText(text: String, font: UIFont? = nil) -> NSAttributedString {
+//  let _font = font ?? UIFont.systemFont(ofSize: 14, weight: .regular)
+//  let fullString = NSMutableAttributedString(string: self, attributes: [NSAttributedString.Key.font: _font])
+//  let boldFontAttribute: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: _font.pointSize)]
+//  let range = (self as NSString).range(of: text)
+//  fullString.addAttributes(boldFontAttribute, range: range)
+//  return fullString
+//}
+//}
+
+
+
+       
+
